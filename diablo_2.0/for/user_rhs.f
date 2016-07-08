a7f348fec4e97db5b278167f878cb230c6ed831c
dwhitt25 <dan.whitt@gmail.com>
2016-07-08 17:04:37 +0100

    Update user_rhs.f

--- diablo_2.0/for/user_rhs.f
+++ diablo_2.0/for/user_rhs.f
@@ -6,7 +6,7 @@
 ! are in Fourier space.  The velocity and scalars are available 
 ! in physical space.
 ! S1 is available as a working variable
-
+! DAN TEST
        integer i,j,k,n
 
        real*8 alpha
